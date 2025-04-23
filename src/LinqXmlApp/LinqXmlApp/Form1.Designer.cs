4ce74468c0dc1e3d74db78a0e88370e837cb19a0
Martin Ortega <lynxestudio@outlook.com>
2025-04-23 00:40:40 -0600

    Updates source code

---
+++ src/LinqXmlApp/LinqXmlApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace LinqXmlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.XmlFile = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.store = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGenerate);
+            this.groupBox1.Controls.Add(this.XmlFile);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 230);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generate Xml";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(13, 24);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(172, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // XmlFile
+            // 
+            this.XmlFile.Location = new System.Drawing.Point(13, 53);
+            this.XmlFile.Multiline = true;
+            this.XmlFile.Name = "XmlFile";
+            this.XmlFile.ReadOnly = true;
+            this.XmlFile.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.XmlFile.Size = new System.Drawing.Size(521, 163);
+            this.XmlFile.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.store);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.txtQuery);
+            this.groupBox2.Location = new System.Drawing.Point(20, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(544, 249);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search Xml";
+            // 
+            // store
+            // 
+            this.store.FormattingEnabled = true;
+            this.store.ItemHeight = 17;
+            this.store.Location = new System.Drawing.Point(6, 57);
+            this.store.Name = "store";
+            this.store.Size = new System.Drawing.Size(515, 157);
+            this.store.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(285, 25);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(79, 24);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(6, 24);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(273, 25);
+            this.txtQuery.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(574, 505);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Linq Xml App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox XmlFile;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox store;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtQuery;
+    }
+}
+
---
+++ src/LinqXmlApp/LinqXmlApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace LinqXmlApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            XElement articulos = new XElement("Articulos",
+    new XElement("Articulo", new XElement("IdArticulo", "1"),
+    new XElement("Codigo", "C01-01-C12140"),
+    new XElement("Nombre", "CATGUT CROMICO 4-0 30CM C1214/2-30")),
+    new XElement("Articulo", new XElement("IdArticulo", "2"),
+    new XElement("Codigo", "C01-01-C26109"),
+    new XElement("Nombre", "ACERO 2 45CM C26109x2")),
+    new XElement("Articulo", new XElement("IdArticulo", "3"),
+    new XElement("Codigo", "C01-01-C40590"),
+    new XElement("Nombre", "POLIESTER 2 75CM C4059")));
+            string s  = articulos.ToString();
+            XmlFile.Text = s;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(XmlFile.Text))
+            {
+                MessageBox.Show("Generate Xml first.");
+            }
+            else
+            {
+                if(string.IsNullOrEmpty(txtQuery.Text))
+                {
+                    MessageBox.Show("Enter a query");
+                }
+                else
+                {
+                    store.Items.Clear();
+                    using (MemoryStream mem = new MemoryStream())
+                    {
+                        byte[] bs = ASCIIEncoding.ASCII.GetBytes(XmlFile.Text);
+                        mem.Write(bs, 0, bs.Length);
+                        mem.Position = 0;
+                        using (StreamReader reader = new StreamReader(mem))
+                        {
+                            XElement xml = XElement.Load(reader);
+                            var q = from s in xml.Elements("Articulo")
+                                    where s.Value.Contains(txtQuery.Text)
+                                    select s;
+
+                            foreach (var r in q)
+                            {
+
+                                XmlReader xmlReader = r.CreateReader();
+                                int i = 0;
+                                string s = null;
+                                while (xmlReader.Read())
+                                {
+
+                                    if (xmlReader.HasValue)
+                                    {
+                                        s += xmlReader.Value + "|";
+                                        i++;
+                                    }
+
+                                }
+                                if (i == 3)
+                                    store.Items.Add(s);
+                            }
+                        }
+                    }//end mem
+                }
+                
+            }
+            
+        }
+    }
+}
